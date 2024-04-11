5653445d5277c08583975ed9a10d7eaf93240ba8
Adeepblue <2631634050@qq.com>
2024-04-11 15:02:19 +0800

    Matlab Source File

--- begin.m
+++ begin.m
@@ -5,17 +5,33 @@ cd SARImageData\
 FixedImage = imread('1999.04.bmp');
 MovingImage = imread("1999.05.bmp");
 
+% FixedImage = rgb2gray(FixedImage);
+% MovingImage = rgb2gray(MovingImage);
+
+
 figure;
 subplot(121)
 imshow(FixedImage)
 title("Fix Image")
 
 subplot(122)
-title("MovingImage")
 imshow(MovingImage)
+title("MovingImage")
 
 [optimizer, metric] = imregconfig('monomodal');
-registeredImage = imregister(MovingImage, FixedImage, 'rigid', optimizer, metric);
+
+[l,w,d] = size(FixedImage);
+
+registeredImage = zeros(l,w,d);
+for Deep = 1:1:3
+registeredImage(:,:,Deep) = imregister(MovingImage(:,:,Deep), FixedImage(:,:,Deep), 'rigid', optimizer, metric);
+end
 
 
+figure
+imshow(uint8(registeredImage));
+title("registeredImage")
+
+cd ..
+cd ..
 
