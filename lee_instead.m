6bdbfbbac9d7936196b4d0ac4fe60028ae1d0058
Adeepblue <2631634050@qq.com>
2024-04-13 12:32:38 +0800

    Matlab Source File

--- lee_instead.m
+++ lee_instead.m
@@ -43,6 +43,13 @@ title("Lee filter")
 
 %% Fine WaveFiltering
 
+clc,clear,close all
+% cd SARImageFile\SARImageData\
+
+Image = imread("yellow_C_1.bmp");
+windowlength = 7;
+Onesidelength = floor(windowlength/2); 
+
 Image_Fine_Filtered = zeros(l-Onesidelength,w-Onesidelength);
 Core1 = [-1,0,1;
          -1,0,1;
@@ -60,6 +67,9 @@ Core4 =[1,1,0;
         1,0,-1;
        0,-1,-1];
 
+CoreTotal = [Core1;Core2;Core3;Core4];
+
+GV = linspace(0,0,4);
 for index1 = 1+Onesidelength:l-Onesidelength
     for index2 = 1+Onesidelength:w-Onesidelength
         temp_window = Image(index1-Onesidelength:index1+Onesidelength,...
@@ -72,7 +82,44 @@ for index1 = 1+Onesidelength:l-Onesidelength
                     mean(reshape(temp_window(2*tempindex1-1:2*tempindex1+1,2*tempindex2-1:2*tempindex2+1),1,[]));
             end
         end
+        
+        for tempindex = 1:4
+            GV(tempindex) = sum(reshape(reshape(CoreTotal(tempindex,:,:),3,3).*M,1,[]));
+        end
 
+        [~,n] = max(GV);
+        switch n
+            case 1 %90
+                if abs(M(2,1)-M(2,2)) > abs(M(2,3)-M(2,2))
+                    temp_window = temp_window(:,4:7);
+                else
+                    temp_window = temp_window(:,1:4);
+                end
+
+            case 2 %135
+                if abs(M(3,1)-M(2,2)) < abs(M(1,3)-M(2,2))
+                    temp_window=[temp_window(1:7,1);temp_window(2:7,2);temp_window(3:7,3);temp_window(4:7,4);temp_window(5:7,5);temp_window(6:7,6);temp_window(7:7,7)];
+                else
+                    temp_window=[temp_window(1:1,1);temp_window(1:2,2);temp_window(1:3,3);temp_window(1:4,4);temp_window(1:5,5);temp_window(1:6,6);temp_window(1:7,7)];
+                end
+
+            case 3 %180
+                if abs(M(1,2)-M(2,2)) < abs(M(3,2)-M(2,2))
+                    temp_window = temp_window(4:7,:);
+                else
+                    temp_window = temp_window(1:4,:);
+                end
+            case 4 %45
+                if abs(M(1,1)-M(2,2)) < abs(M(3,3)-M(2,2))
+                    temp_window=[temp_window(1:7,1);temp_window(1:6,2);temp_window(1:5,3);temp_window(1:4,4);temp_window(1:3,5);temp_window(1:2,6);temp_window(1:1,7)];
+                else
+                    temp_window=[temp_window(7:7,1);temp_window(6:7,2);temp_window(5:7,3);temp_window(4:7,4);temp_window(3:7,5);temp_window(2:7,6);temp_window(1:7,7)];
+                end
+        end
         
     end
 end
+
+clear tempindex1 tempindex2 tempindex
+
+
