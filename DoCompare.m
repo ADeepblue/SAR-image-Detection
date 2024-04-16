e31c1397e3b732708277d9fecd714b684e044a32
Adeepblue <2631634050@qq.com>
2024-04-16 21:45:37 +0800

    Matlab Source File

--- DoCompare.m
+++ DoCompare.m
@@ -4,26 +4,37 @@ Image1 = imread("SARImageFile\FilterImageFile\leefilterbeijing_A_1.jpg");
 Image2 = imread("SARImageFile\FilterImageFile\leefilterbeijing_A_2.jpg");
 CompareResult1 = Image1 ./ Image2;
 CompareResult2 = Image2 ./ Image1;
-
+CompareResult3 = Image1 -Image2;
+CompareResult4 = Image2 -Image1;
 %% figure
 
 magnification = 50;
 
 figure
-subplot(223)
+subplot(233)
 imshow(CompareResult1*magnification);
 title("Image1/Image2")
 disp(max(CompareResult2(:)))
 
-subplot(224)
+subplot(234)
 imshow(CompareResult2*magnification);
 title("Image2/Image1")
-disp(max(CompareResult2(:)))
 
-subplot(221)
+
+subplot(231)
 imshow(Image1);
 title("Image1")
 
-subplot(222)
+subplot(232)
 imshow(Image2);
 title("Image2")
+
+subplot(235)
+imshow(CompareResult3*magnification);
+title("Image1-Image2")
+
+
+subplot(236)
+imshow(CompareResult4*magnification);
+title("Image2-Image1")
+
--- GGbegin.m
+++ GGbegin.m
@@ -1,7 +1,8 @@
 clc,clear,close all
 myu = 0;
-alpha = 0;
-beta = 0;
-syms sita x
+alpha = 1;
+beta = 1;
+syms x
 
 P = beta/(2*alpha*gamma(1/beta))*exp(-(abs(x-myu)/alpha).^beta);
+fplot(x,P)
